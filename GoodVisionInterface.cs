831678ed807678bcd04963c1f197de8ea7e864cd
Ap0ll0n <xxxsokolromeoxxx@gmail.com>
2019-10-18 15:27:16 +0300

    Добавьте файлы проекта.

---
+++ GoodVisionInterface.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodVision
+{
+    interface GoodVisionInterface
+    {
+        void Create_account(string Nickname);
+        void Clear_account(UserClass User);
+
+        void Enter_account();
+
+        void Get_Stats(UserClass User);
+        void Test_vision_with_letters(UserClass User);
+        void Test_vision_with_circles(UserClass User);
+
+        void Settings();
+        void Get_help();
+
+    }
+}
---
+++ UserClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodVision
+{
+    class UserClass
+    {
+        private string Nickname;
+        private DateTime last_check_date;
+        private double left_eye_vision;
+        private double right_eye_vision;
+        public UserClass(UserClass User)
+        {
+            this.Nickname = User.Nickname;
+            this.left_eye_vision = User.left_eye_vision;
+            this.right_eye_vision = User.right_eye_vision;
+            this.last_check_date = User.last_check_date;
+        }
+        public string Nick { get; set; }
+        public double get_left_eye_vision(UserClass User)
+        {
+            return User.left_eye_vision;
+        }
+        public double get_right_eye_vision(UserClass User)
+        {
+            return User.right_eye_vision;
+        }
+        public Tuple<double, double> get_vision(UserClass User)
+        {
+            var vision = new Tuple<double, double>(User.left_eye_vision, User.right_eye_vision);
+
+            return vision;
+        }
+    }
+}
