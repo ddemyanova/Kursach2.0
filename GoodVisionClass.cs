75503f353961c55eea0b8266f4a96bb950ec8174
ddemyanova <d.demyanova.d@gmail.com>
2019-10-20 23:59:37 +0300

    add GV(class)
    Добавила класс GoodVision и начала писать методы
    в UserСlass добавила функции get/set для полей

---
+++ GoodVisionClass.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace GoodVision
+{
+	class GoodVisionClass: GoodVisionInterface
+	{
+		private FileStream fileStream;
+		void Create_account(string Nickname)
+		{
+			string filePath = Nickname + ".xml";//можно прописать тут полный адрес
+			if (File.Exists(filePath))
+			{
+				//вывести свой месседж бокс с "да-нет",сказать, что уже существует такой юзер
+				//если это вы - Enter Account();
+				//если нет - запросить снова
+				//хз что здесь возвращать
+			}
+			else
+			{
+				fileStream = new FileStream(filePath, FileMode.Create, FileAccess.ReadWrite);
+				//создать файл с именем;
+				
+			}
+
+			
+		}
+		void Enter_account(string Nickname)
+		{
+			string filePath = Nickname + ".xml";//можно прописать тут полный адрес
+			if (File.Exists(filePath))
+			{
+			fileStream = new FileStream(filePath, FileMode.Open, FileAccess.ReadWrite);
+			}
+			//открыть файл ;
+			else
+			{
+				//если такого нет - вывести месседж бокс с ответом "ок", что нет такого пользователя и предложить создать нового
+				//Create_account();
+				Create_account(Nickname);
+			}
+			
+		}
+		
+	}
+}
