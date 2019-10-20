5fb456db76e23501fa2abc24121915c305a16a0d
Ap0ll0n <xxxsokolromeoxxx@gmail.com>
2019-10-20 18:23:43 +0300

    Почти доделал класс Letter

---
+++ Letter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodVision
+{
+    class Letter : Object
+    {
+        private enum Letters
+        {
+            Ш = 1,
+            Б,
+            М,
+            Н,
+            К,
+            Ы,
+            И
+        };
+    }
+}
--- Object.cs
+++ Object.cs
@@ -8,16 +8,39 @@ using System.Drawing;
 
 namespace GoodVision
 {
-   abstract class Object
+
+    abstract class Object
     {
-        private Image Image_to_show = Image.FromFile("Ssss.jpg");//рандомное имя файла
-        
-            public Tuple<double,double> Get_size(Image Image_to_show)
+        private string FileName;
+        private Tuple<double, double> size;
+        private Image ShowImage;
+        public Object()
         {
-            var size = new Tuple<double, double>((double)Image_to_show.Height * 0.265, (double)Image_to_show.Width * 0.265);
+            this.FileName = "...";//Тут должен будет быт путь к базовому изображения
+            this.size = new Tuple<double, double>(2.33333333, 2.33333333);//размер изображения в милиметрах для 6(базовой строки)
+            this.ShowImage = Image.FromFile(this.FileName);
+        }
+        public Object(Object Obj)
+        {
+            this.FileName = Obj.FileName;
+            ShowImage = Image.FromFile(this.FileName);
+            this.size = Obj.size;
+        }
+        virtual public Tuple<double, double> Get_size(Image Image_to_show)
+        {
+            this.size = new Tuple<double, double>((double)Image_to_show.Height * 0.265, (double)Image_to_show.Width * 0.265);
             return size;
         }
 
-      //  public void Set_size(Image Image_to_show)
-    }
+        virtual public void Set_size(Tuple<double, double> Size)
+        {
+            this.size = Size;
+        }
+        ~Object()
+        {
+            //хз че в деструкторе писать
+        }
+    };
 }
+
+
